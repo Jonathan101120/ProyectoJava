7ab9856b08160e29800ef53654dbbfa4bc147a0c
Johancito <96688861+Jonathan101120@users.noreply.github.com>
2023-07-29 01:12:13 -0500

    Mejoras para el usuario

--- src/proyect1.java
+++ src/proyect1.java
@@ -14,6 +14,8 @@ public class proyect1 {
         int puntos_Persona = 0;
         int puntos_IA = 0;
         int[] lista_puntos_persona = new int[100];
+        int[] lista_puntos_IA = new int[100];
+        int i=0,j=0;
 
         while(puntos_Persona<100 || puntos_IA <100){
             Scanner sc = new Scanner(System.in);
@@ -21,15 +23,38 @@ public class proyect1 {
             String continuar = sc.nextLine();
 
             if(continuar.equals("Si") || continuar.equals("si")){
-                int i=0;
                 int resultado_dado = dado();
-                lista_puntos_persona[i] = resultado_dado;
-                puntos_Persona += resultado_dado;
-                i++;
+                int puntos_per=0;
+                puntos += resultado_dado;
+                if(resultado_dado == 1){
+                    lista_puntos_persona[i] = 0;
+                    puntos_Persona -= puntos_per;
+                    continuar = "No";
+                }
+                else {
+                    lista_puntos_persona[i] = resultado_dado;
+                    puntos_Persona += resultado_dado;
+                    i++;
+                }
             }
+            else{
+                int resulado_dado_IA = dado();
+                int turno_IA = 0;
+                int puntos_acumulados_IA = 0;
+                while(turno_IA <= 3){
+                    if (resulado_dado_IA == 1){
+                        lista_puntos_persona[j] = 0;
 
+                        turno_IA=4;
+                    }
+                    else
+                        lista_puntos_persona[j] = resulado_dado_IA;
 
-            puntos_IA += dado();
+                    puntos_IA += resulado_dado_IA;
+                    turno_IA++;
+                    j++;
+                }
+            }
 
         }
     }
